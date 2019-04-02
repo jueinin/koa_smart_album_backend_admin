9374570bb7c21e156e7cd22dce51e63a3b580eb1
jueinin <1014397160@qq.com>
2019-04-02 23:28:30 +0800

    修改数据库

--- src/tools/sequelizeGenerator.ts
+++ src/tools/sequelizeGenerator.ts
@@ -3,11 +3,12 @@ import Sequelize from 'sequelize'
 let sequelize = new Sequelize.Sequelize({
   host: "localhost",
   dialect: "mysql",
-  database: "newbee_smart_album",
+  //database: "newbee_smart_album",
+  database: "smart_album",
   username: "root",
   password: "GYf51610532",
-  define:{
+  define: {
     timestamps: false,
   }
-})
+});
 export default sequelize;
